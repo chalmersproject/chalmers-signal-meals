33594a151a6f601a898344c20871c74dfd69ab5a
Chalmers <chalmers@chalmers-book.local>
2019-09-01 18:14:47 -0400

    refactoring for OOP

--- include/objects.h
+++ include/objects.h
@@ -2,7 +2,27 @@ using namespace std;
 
 class Position 
 {
-    public:
+    const byte pin;
+    int state;
+    unsigned long time_since_change;
     int lights[4];
     bool active;
+
+    public:
+        Position(byte pinAttach, int stateAttach, int lightsAttach[4]):
+            pin(pinAttach),
+            state(stateAttach),
+            lights(lightsAttach)
+        {
+
+        }
+
+    void setup()
+    {
+        pinMode(pin, INPUT_PULLUP)
+    }
+    void loop()
+    {
+        time_since_change = millis();
+    }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,8 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>  // Enables the ESP8266 to connect to the local network (via WiFi)
 #include <FastLED.h>
-
 #include <FirebaseArduino.h>
+
 //my own header files
 #include <variables.h>
 #include <credentials.h>
@@ -11,17 +11,24 @@
 #include <objects.h>
 CRGB leds[NUM_LEDS];
 FASTLED_USING_NAMESPACE
+
+Position no_meal(no_meal, [0,1,2,3], 0);
+Position meal;  
+Position snacks;
+
 void setup() {
   delay(3000);
   setup_wifi();
   setup_firebase();
   setup_leds();
-  
-  Position no_meal;
-  Position meal;  
-  Position snacks;
 
-  init_positions(no_meal, [0,1,2,3], false);
+  //init the positions
+  no_meal.setup;
+  meal.setup;
+  snacks.setup
+  
+
+  init_positions();
   init_positions(meal, [4,5,6,7], false);
   init_positions(snacks, [8,9,10,11], false);
 }
